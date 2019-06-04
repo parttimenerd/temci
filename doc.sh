1d31a0bcb98863870a09be3b19b4bc423fb8f7be
Johannes Bechberger <johannes.bechberger@kit.edu>
2019-06-04 17:52:30 +0200

    Fix doc.sh script
    It now generates proper documentation

--- doc.sh
+++ doc.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 # Generates the documentation for temci
 
-sphinx-apidoc -o doc -F -H "temci" -A "Johannes Bechberger" -V `temci version` temci -d 100
+SPHINXDOC=True sphinx-apidoc -o doc -F -H "temci" -A "Johannes Bechberger" -V `temci version` temci -d 100
 cd doc; make html
