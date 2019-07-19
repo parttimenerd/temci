a6a4afa1963b778acd0d952847159a4965baf4ec
Johannes Bechberger <johannes.bechberger@kit.edu>
2019-07-19 10:49:21 +0200

    Add test for typecheck

--- tests/test_typecheck.py
+++ tests/test_typecheck.py
@@ -1,7 +1,7 @@
 """
 Tests related to the typecheck code
 """
-from temci.utils.typecheck import verbose_isinstance, Int, Dict
+from temci.utils.typecheck import verbose_isinstance, Int, Dict, Either, T, List
 
 
 def test_dict_missing_key_error_msg():
@@ -10,3 +10,7 @@ def test_dict_missing_key_error_msg():
 
 def test_dict_wrong_type_error_msg():
     assert verbose_isinstance({"a": "s"}, Dict({"a": Int()})).msg.lower().startswith("'s'")
+
+
+def test_either_ints():
+    assert isinstance(28593,  Either(Either(Int()|T(float))|List(Either(Int()|T(float)))))
